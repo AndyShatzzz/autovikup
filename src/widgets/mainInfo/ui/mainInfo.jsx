cfc2611acf34a024af3399060ef2182d93c6b27e
Andrey Shatskikh <ponchic-09@mail.ru>
2025-03-14 16:49:42 +0300

    feat(favicon): add favicon.ico

--- src/widgets/mainInfo/ui/mainInfo.jsx
+++ src/widgets/mainInfo/ui/mainInfo.jsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import React, { useState } from "react";
 import styles from "./mainInfo.module.scss";
 import FeedBack from "@/entities/feedBack/ui/feedBack";
